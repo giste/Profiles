816d92c9939475294449eec76a779757f2c00cba
Giste <giste.trappiste@gmail.com>
2022-09-23 11:25:32 +0200

    Make ProfileDetail default settings exhaustive.

--- app/src/main/java/org/giste/profiles/domain/ProfileDetail.kt
+++ app/src/main/java/org/giste/profiles/domain/ProfileDetail.kt
@@ -3,22 +3,12 @@ package org.giste.profiles.domain
 data class ProfileDetail(
     val id: Long = 0,
     val name: String = "",
-    val settings: Map<SettingType, Setting<Any>> = mapOf(
-        SettingType.VOLUME_MEDIA to IntSetting(
-            profileId = id,
-            type = SettingType.VOLUME_MEDIA
-        ),
-        SettingType.VOLUME_RING to IntSetting(
-            profileId = id,
-            type = SettingType.VOLUME_RING
-        ),
-        SettingType.VOLUME_NOTIFICATION to IntSetting(
-            profileId = id,
-            type = SettingType.VOLUME_NOTIFICATION
-        ),
-        SettingType.VOLUME_ALARM to IntSetting(
-            profileId = id,
-            type = SettingType.VOLUME_ALARM
-        ),
-    )
+    val settings: Map<SettingType, Setting<Any>> = SettingType.values().associate {
+        when (it) {
+            SettingType.VOLUME_MEDIA,
+            SettingType.VOLUME_RING,
+            SettingType.VOLUME_NOTIFICATION,
+            SettingType.VOLUME_ALARM -> Pair(it, IntSetting(profileId = id, type = it))
+        }
+    }
 )
