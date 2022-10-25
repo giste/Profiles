1e9fb02696ff4820cf2a12fb87ac91cee538eb7a
Giste <giste.trappiste@gmail.com>
2022-10-25 19:41:00 +0200

    Change WiFi and ringer mode.

--- app/src/main/java/org/giste/profiles/data/SystemSettingsDataSource.kt
+++ app/src/main/java/org/giste/profiles/data/SystemSettingsDataSource.kt
@@ -3,6 +3,7 @@ package org.giste.profiles.data
 import android.content.Context
 import android.media.AudioManager
 import android.net.wifi.WifiManager
+import android.util.Log
 import org.giste.profiles.domain.RingModeSetting
 import org.giste.profiles.domain.Setting
 import org.giste.profiles.domain.SettingType
@@ -17,21 +18,16 @@ class SystemSettingsDataSource @Inject constructor(context: Context) {
             .forEach {
                 when (it.type) {
                     SettingType.VOLUME_MEDIA ->
-                        am.setStreamVolume(AudioManager.STREAM_MUSIC, it.value as Int, 0)
+                        setVolume(AudioManager.STREAM_MUSIC, it.value as Int)
                     SettingType.VOLUME_RING ->
-                        am.setStreamVolume(AudioManager.STREAM_RING, it.value as Int, 0)
+                        setVolume(AudioManager.STREAM_RING, it.value as Int)
                     SettingType.VOLUME_NOTIFICATION ->
-                        am.setStreamVolume(AudioManager.STREAM_NOTIFICATION, it.value as Int, 0)
+                        setVolume(AudioManager.STREAM_NOTIFICATION, it.value as Int)
                     SettingType.VOLUME_ALARM ->
-                        am.setStreamVolume(AudioManager.STREAM_ALARM, it.value as Int, 0)
-                    SettingType.RING_MODE -> {}
-//                        am.ringerMode =
-//                        when (it.value as RingModeSetting.Companion.RingMode) {
-//                            RingModeSetting.Companion.RingMode.NORMAL -> AudioManager.RINGER_MODE_NORMAL
-//                            RingModeSetting.Companion.RingMode.VIBRATE -> AudioManager.RINGER_MODE_VIBRATE
-//                            RingModeSetting.Companion.RingMode.SILENT -> AudioManager.RINGER_MODE_SILENT
-//                        }
-                    SettingType.CONNECTION_WIFI -> {} //applyWiFi(it.value as Boolean)
+                        setVolume(AudioManager.STREAM_ALARM, it.value as Int)
+                    SettingType.RING_MODE ->
+                        setRingerMode(it.value as RingModeSetting.Companion.RingMode)
+                    SettingType.CONNECTION_WIFI -> applyWiFi(it.value as Boolean)
                     SettingType.CONNECTION_DATA -> {}
                     SettingType.CONNECTION_BLUETOOTH -> {}
                     SettingType.CONNECTION_NFC -> {}
@@ -43,8 +39,24 @@ class SystemSettingsDataSource @Inject constructor(context: Context) {
             }
     }
 
+    private fun setVolume(stream: Int, value: Int) {
+        Log.d("SystemSettingsDataSource", "Setting volume($stream) = $value")
+        am.setStreamVolume(stream, value, 0)
+    }
+
+    private fun setRingerMode(mode: RingModeSetting.Companion.RingMode) {
+        Log.d("SystemSettingsDataSource", "Setting ringer mode = $mode")
+        am.ringerMode = when (mode) {
+            RingModeSetting.Companion.RingMode.NORMAL -> AudioManager.RINGER_MODE_NORMAL
+            RingModeSetting.Companion.RingMode.VIBRATE -> AudioManager.RINGER_MODE_VIBRATE
+            RingModeSetting.Companion.RingMode.SILENT -> AudioManager.RINGER_MODE_SILENT
+        }
+    }
+
     private fun applyWiFi(value: Boolean) {
         if (wm.isWifiEnabled != value) {
+            Log.d("SystemSettingsDataSource", "Setting WiFi = $value")
+            @Suppress("DEPRECATION")
             wm.isWifiEnabled = value
         }
     }
